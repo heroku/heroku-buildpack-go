a67f5d003a0a6b35660093c19366dc69ad839589
Keith Rarick <kr@xph.us>
2011-10-12 15:41:36 -0700

    fix outdated test

--- detect-test.sh
+++ detect-test.sh
@@ -6,17 +6,11 @@ after() {
   rm -rf detect-test
 }
 
-it_is_go_if_all_sh() {
-  sh -x bin/detect detect-test/
-}
-
 it_is_go_if_go_files_under_src() {
-  rm -rf detect-test/all.sh 
   sh -x bin/detect detect-test/
 }
 
 it_is_not_go_without_all_sh_or_go_files() {
-  rm -rf detect-test/all.sh
   rm -rf detect-test/src/*
   ! sh -x bin/detect detect-test/
 }
