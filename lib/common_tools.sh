31140215a15286df6446073aa28ad4a34f536eab
Michael Shea <mike.shea@gmail.com>
2019-05-09 13:21:58 +1000

    Use mktemp to create a temporary directory
    Do this only if TMPDIR is unset. If TMPDIR is set, use $TMPDIR/go-buildpack-stdlib

--- lib/common_tools.sh
+++ lib/common_tools.sh
@@ -4,5 +4,9 @@
 ensureInPath "jq-linux64" "${cache}/.jq/bin"
 
 # Ensure we have a copy of the stdlib
-STDLIB_DIR="${TMPDIR:-"/tmp"}/go-buildpack-stdlib"
+if [ -z "${TMPDIR}" ]; then
+  STDLIB_DIR=$(mktemp -d -t stdlib.XXXXX)
+else
+  STDLIB_DIR="${TMPDIR}/go-buildpack-stdlib"
+fi
 ensureFile "stdlib.sh.v8" "${STDLIB_DIR}" "chmod a+x"
