83c281393f8680dec6561ee1074c21e7f1bb6e8c
Edward Muller <emuller@salesforce.com>
2019-05-09 10:03:28 -0700

    Switch to path based sourcing of stdlib.sh.v8
    sourcing is 1 way wrt enviornment vars unless they're exported and I
    don't want to start sprinking "exports" around.

--- lib/common_tools.sh
+++ lib/common_tools.sh
@@ -3,10 +3,5 @@
 # Ensure jq is installed.
 ensureInPath "jq-linux64" "${cache}/.jq/bin"
 
-# Ensure we have a copy of the stdlib
-if [ -z "${TMPDIR}" ]; then
-  STDLIB_DIR=$(mktemp -d -t stdlib.XXXXX)
-else
-  STDLIB_DIR="${TMPDIR}/go-buildpack-stdlib"
-fi
-ensureFile "stdlib.sh.v8" "${STDLIB_DIR}" "chmod a+x"
+# Ensure we have a copy of stdlib.sh.v8 in the path
+ensureInPath "stdlib.sh.v8" "$(mktemp -d -t go-buildpack-stdlib.XXXXX)" "chmod a+x"
