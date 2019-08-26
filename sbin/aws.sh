56adcd00745325781d9434275908c5c92ed57f05
Edward Muller <emuller@salesforce.com>
2019-08-26 11:25:10 -0700

    Use the named item instead of the id.
    This should make it easier to find the item in LastPass if/when that is
    needed.

--- sbin/aws.sh
+++ sbin/aws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # source this file
-export AWS_ACCESS_KEY_ID="$(lpass show --sync=now --notes 9022891142845286058 | jq -r '.AccessKey | .AccessKeyId')"
-export AWS_SECRET_ACCESS_KEY="$(lpass show --sync=now --notes 9022891142845286058 | jq -r '.AccessKey | .SecretAccessKey')"
+export AWS_ACCESS_KEY_ID="$(lpass show --sync=now --notes "Shared-Go Language/Go S3 Bucket" | jq -r '.AccessKey | .AccessKeyId')"
+export AWS_SECRET_ACCESS_KEY="$(lpass show --sync=now --notes "Shared-Go Language/Go S3 Bucket" | jq -r '.AccessKey | .SecretAccessKey')"
