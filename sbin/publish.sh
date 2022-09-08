497372f0b055520fd17ec0b2169782e550f44443
Josh W Lewis <josh.lewis@salesforce.com>
2022-09-08 17:01:44 -0500

    Fixup release version history (#499)
    * Update publish script to match other buildpacks (and fetch upstream)
    
    * Update changelog to reflect actual buildpack registry history

--- sbin/publish.sh
+++ sbin/publish.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 BP_NAME=${1:-"heroku/go"}
 
 curVersion=$(heroku buildpacks:versions "$BP_NAME" | awk 'FNR == 3 { print $1 }')
 newVersion="v$((curVersion + 1))"
 
-read -r -p "Deploy as version: $newVersion [y/n]? " choice
+read -p "Deploy as version: $newVersion [y/n]? " choice
 case "$choice" in
   y|Y ) echo "";;
   n|N ) exit 0;;
   * ) exit 1;;
 esac
 
+git fetch origin
 originMain=$(git rev-parse origin/main)
 echo "Tagging commit $originMain with $newVersion... "
 git tag "$newVersion" "${originMain:?}"
 git push origin refs/tags/$newVersion
 
+echo -e "\nPublishing to the buildpack registry..."
 heroku buildpacks:publish "$BP_NAME" "$newVersion"
-
-echo "Done."
+echo
+heroku buildpacks:versions "${BP_NAME}" | head -n 3
