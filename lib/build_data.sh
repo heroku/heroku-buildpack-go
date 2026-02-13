bdc87b17fed6b84c71024232e872b456d041498c
Rune Soerensen <rsoerensen@salesforce.com>
2026-02-13 02:14:04 -0500

    Add `lib/build_data.sh`
    Adapted from https://github.com/heroku/heroku-buildpack-python/blob/8eaa83cf0f9e196abd13e895bc667274183909a6/lib/build_data.sh

---
+++ lib/build_data.sh
@@ -0,0 +1,176 @@
+#!/usr/bin/env bash
+
+# Note: -u is omitted for now so we don't break non-strict callers.
+set -eo pipefail
+
+BUILD_DATA_FILE="${CACHE_DIR:?}/build-data/go.json"
+PREVIOUS_BUILD_DATA_FILE="${CACHE_DIR:?}/build-data/go-prev.json"
+
+# Initializes the build data store, preserving the file from the previous build if it exists.
+# Call this at the start of `bin/compile` before using any other functions from this file.
+#
+# Usage:
+# ```
+# build_data::setup
+# ```
+function build_data::setup() {
+	if [[ -f "${BUILD_DATA_FILE}" ]]; then
+		# Rename the existing build data file rather than overwriting it, so we can lookup values
+		# from the previous build (such as when determining whether to invalidate the cache).
+		mv "${BUILD_DATA_FILE}" "${PREVIOUS_BUILD_DATA_FILE}"
+	else
+		mkdir -p "$(dirname "${BUILD_DATA_FILE}")"
+	fi
+
+	echo "{}" >"${BUILD_DATA_FILE}"
+}
+
+# Sets a string build data value. The value will be wrapped in double quotes and escaped for JSON.
+#
+# Usage:
+# ```
+# build_data::set_string "python_version" "1.2.3"
+# build_data::set_string "failure_reason" "install-dependencies::pip"
+# ```
+function build_data::set_string() {
+	local key="${1}"
+	local value="${2}"
+	build_data::_set "${key}" "${value}" "true"
+}
+
+# Sets a build data value for the elapsed time in seconds between the provided start time and the
+# current time, represented as a float with microseconds precision.
+#
+# Usage:
+# ```
+# local dependencies_install_start_time=$(build_data::current_unix_realtime)
+# # ... some operation ...
+# build_data::set_duration "dependencies_install_duration" "${dependencies_install_start_time}"
+# ```
+function build_data::set_duration() {
+	local key="${1}"
+	local start_time="${2}"
+	local end_time duration
+	end_time="$(build_data::current_unix_realtime)"
+	duration="$(awk -v start="${start_time}" -v end="${end_time}" 'BEGIN { printf "%f", (end - start) }')"
+	build_data::set_raw "${key}" "${duration}"
+}
+
+# Sets a build data value as raw JSON data. The value parameter must be valid JSON value, that's also
+# a supported Honeycomb data type (string, integer, float, or boolean only; no arrays or objects).
+# For strings, use `build_data::set_string` instead since it will handle the escaping/quoting for you.
+# And for durations, use `build_data::set_duration`.
+#
+# Usage:
+# ```
+# build_data::set_raw "python_version_outdated" "true"
+# build_data::set_raw "foo_size_mb" "42.5"
+# ```
+function build_data::set_raw() {
+	local key="${1}"
+	local value="${2}"
+	build_data::_set "${key}" "${value}" "false"
+}
+
+# Internal helper to write a key/value pair to the build data store. The buildpack shouldn't call this directly.
+# Takes a key, value, and a boolean flag indicating whether the value needs to be quoted.
+#
+# Usage:
+# ```
+# build_data::_set "foo_string" "quote me" "true"
+# build_data::_set "bar_number" "99" "false"
+# ```
+function build_data::_set() {
+	local key="${1}"
+	# Truncate the value to an arbitrary 200 characters since it will sometimes contain user-provided
+	# inputs which may be unbounded in size. Ideally individual call sites will perform more aggressive
+	# truncation themselves based on the expected value size, however this is here as a fallback.
+	# (Honeycomb supports string fields up to 64KB in size, however, it's not worth filling up the
+	# build data store or bloating the payload passed back to Vacuole/submitted to Honeycomb given the
+	# extra content in those cases is not normally useful.)
+	local value="${2:0:200}"
+	local needs_quoting="${3}"
+
+	if [[ "${needs_quoting}" == "true" ]]; then
+		# Values passed using `--arg` are treated as strings, and so have double quotes added and any JSON
+		# special characters (such as newlines, carriage returns, double quotes, backslashes) are escaped.
+		local jq_args=(--arg value "${value}")
+	else
+		# Values passed using `--argjson` are treated as raw JSON values, and so aren't escaped or quoted.
+		local jq_args=(--argjson value "${value}")
+	fi
+
+	if [[ -f "${BUILD_DATA_FILE}" ]]; then
+		local new_data_file_contents
+		new_data_file_contents="$(jq --exit-status --arg key "${key}" "${jq_args[@]}" '. + { ($key): ($value) }' "${BUILD_DATA_FILE}")"
+		echo "${new_data_file_contents}" >"${BUILD_DATA_FILE}"
+	else
+		err "Error: Can't find the buildpack's build data file."
+        err ""
+		err "The Go buildpack's internal build data file is missing:"
+        err "${BUILD_DATA_FILE}"
+        err ""
+		err "This file is required for the buildpack to work correctly,"
+		err "and so you must not delete it when removing files from the"
+		err "build cache or /tmp directories."
+		build_data::setup
+		build_data::set_string "failure_reason" "build-data::data-file-deleted"
+		exit 1
+	fi
+}
+
+# Check whether an entry exists in the build data store for the current build.
+# Returns zero if the key was found and non-zero otherwise.
+#
+# Usage:
+# ```
+# build_data::has "failure_reason"
+# ```
+function build_data::has() {
+	local key="${1}"
+
+	jq --exit-status "has(\"${key}\")" "${BUILD_DATA_FILE}" >/dev/null
+}
+
+# Retrieve the value of an entry in the build data store from the previous successful build.
+# Returns the empty string if the key wasn't found in the store.
+#
+# Usage:
+# ```
+# build_data::get_previous "python_version"
+# ```
+function build_data::get_previous() {
+	local key="${1}"
+
+	if [[ -f "${PREVIOUS_BUILD_DATA_FILE}" ]]; then
+		# The `// empty` ensures we return the empty string rather than `null` if the key doesn't exist.
+		# We don't use `--exit-status` since `false` is a valid value for us to retrieve.
+		jq --raw-output ".${key} // empty" "${PREVIOUS_BUILD_DATA_FILE}"
+	fi
+}
+
+# Returns the current time since the UNIX Epoch, as a float with microseconds precision.
+#
+# Usage:
+# ```
+# local dependencies_install_start_time=$(build_data::current_unix_realtime)
+# # ... some operation ...
+# build_data::set_duration "dependencies_install_duration" "${dependencies_install_start_time}"
+# ```
+function build_data::current_unix_realtime() {
+	# We use a subshell with `LC_ALL=C` to ensure the output format isn't affected by system locale.
+	(
+		LC_ALL=C
+		echo "${EPOCHREALTIME}"
+	)
+}
+
+# Prints the contents of the build data store in sorted JSON format.
+#
+# Usage:
+# ```
+# build_data::print_bin_report_json
+# ```
+function build_data::print_bin_report_json() {
+	jq --exit-status --sort-keys '.' "${BUILD_DATA_FILE}"
+}
