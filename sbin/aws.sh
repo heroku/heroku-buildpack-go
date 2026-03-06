bd5dac7beb3f8aed22d40a701fb2e7ea5fa03f18
Rune Soerensen <rsoerensen@salesforce.com>
2026-03-06 15:05:45 -0500

    Remove S3 sync infrastructure

--- sbin/aws.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# source this file
-export AWS_ACCESS_KEY_ID="$(op read "op://Heroku Languages Team/y6etv4np7575qb6ykgb6wqcnjy/notesPlain" | jq -r '.AccessKey | .AccessKeyId')"
-export AWS_SECRET_ACCESS_KEY="$(op read "op://Heroku Languages Team/y6etv4np7575qb6ykgb6wqcnjy/notesPlain" | jq -r '.AccessKey | .SecretAccessKey')"
